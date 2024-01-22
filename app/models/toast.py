33b39936d76c44e2d1d637c1d9a324d1cc113b49
Hillary Jackson <hill_a_ry@hotmail.com>
2024-01-22 12:31:09 -0500

    starter for toasts

--- app/models/toast.py
+++ app/models/toast.py
@@ -6,8 +6,11 @@ class Toast(db.Model):
     id= db.Column(db.Integer, nullable=False, primary_key=True)
     title= db.Column(db.String, nullable=False)
     body = db.Column(db.Text, nullable=False)
-    # imate_url = db.URL
-    user_id
-    subbreadit_id
-    created_at
-    updated_at
+    image_url = db.Column(db.URL)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    subbreadit_id = db.Column(db.Integer, db.ForeignKey('subbreadit.id'), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+    updated_at = db.Column(db.DateTime)
+
+    user = db.relationship('User', back_populates="toast")
+    subbreadit = db.relationship('Subbreadit', back_populates="toast")
