0132c2ffbc18717ea6e61d3d018e4132c2d0984a
juniper <jjlazo96@gmail.com>
2024-01-26 18:22:26 -0500

    debugging

--- migrations/env.py
+++ migrations/env.py
@@ -21,7 +21,7 @@ config = context.config
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 fileConfig(config.config_file_name)
-logger = logging.getLogger('alembic.env')
+logger = logging.getLogger('alembic.ini')
 
 # add your model's MetaData object here
 # for 'autogenerate' support
