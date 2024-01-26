36863bef9513934fc8346a7456a9e1ef26c0a6f1
juniper <jjlazo96@gmail.com>
2024-01-26 17:39:07 -0500

    prod issues

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -19,10 +19,10 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_users()
-        undo_subbreadits()
-        undo_toasts()
         undo_comments()
+        undo_toasts()
+        undo_subbreadits()
+        undo_users()
     seed_users()
     seed_subbreadits()
     seed_toasts()
@@ -33,8 +33,8 @@ def seed():
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
-    undo_users()
-    undo_subbreadits()
-    undo_toasts()
     undo_comments()
+    undo_toasts()
+    undo_subbreadits()
+    undo_users()
     # Add other undo functions here
