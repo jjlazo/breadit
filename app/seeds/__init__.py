a67bb5af88f28d7c40dcab2161c45ba883797137
juniper <jjlazo96@gmail.com>
2024-01-26 17:45:31 -0500

    prod bug

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -19,10 +19,11 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_comments()
-        undo_toasts()
-        undo_subbreadits()
-        undo_users()
+        db.session.execute(f"TRUNCATE table {SCHEMA}.comments RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.toasts RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.subbreadits RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+        db.session.commit()
     seed_users()
     seed_subbreadits()
     seed_toasts()
